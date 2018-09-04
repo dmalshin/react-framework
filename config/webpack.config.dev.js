a13bfa89a9c7bb49cdee8c85a77310c462ea8859
Dmitry Alshin <dm.alshin@gmail.com>
2018-09-04 13:59:49 +0300

    Fix scss transpiling

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -167,9 +167,6 @@ module.exports = {
                 },
               },
               {
-                loader: require.resolve('sass-loader'),
-              },
-              {
                 loader: require.resolve('postcss-loader'),
                 options: {
                   // Necessary for external CSS imports to work
@@ -200,6 +197,9 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                 },
+              },
+              {
+                loader: require.resolve('sass-loader'),
               },
               {
                 loader: require.resolve('postcss-loader'),
