41eec058b075eeb6471caa7f913234a34c94df8a
Dmitry Alshin <dm.alshin@gmail.com>
2018-09-04 13:11:22 +0300

    Добавлена структура папок и файлов

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import HttpClient from '../core/HttpClient'
+
+// Transitions
+// export const getTask = () => HttpClient.get('/task')
+// export const createTask = (fields) => HttpClient.post(`/task/create`, fields)
+// export const updateTask = (id, fields) => HttpClient.put(`/task/${id}`, fields)
+// export const deleteTask = (id) => HttpClient.delete(`/task/${id}`)
+
+// Types
+// export const getTask = () => HttpClient.get('/task')
+// export const createTask = (fields) => HttpClient.post(`/task/create`, fields)
+// export const updateTask = (id, fields) => HttpClient.put(`/task/${id}`, fields)
+// export const deleteTask = (id) => HttpClient.delete(`/task/${id}`)
