d6a31107d004089544b17f0828e58566bf55c4a9
Альшин Дмитрий Александрович <DAlshin@ibs.root.ibscorp.ru>
2018-10-08 11:56:44 +0300

    Добавлена обработка ошибок при рендеринге

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { Route } from 'react-router-dom'
 import * as routes from '.'
 
 export const AppRoutes = () => (
-  <React.Fragment>
+  // можно использовать ComponentError из ui-components
+  // <ErrorBoundary FallbackComponent={ComponentError}>
+  <ErrorBoundary>
     <Route exact path="/" component={routes.HomeRoute} />
     <Route path="/ui-components" component={routes.UIComponentsRoute} />
     <Route path="/form" component={routes.FormRoute} />
-  </React.Fragment>
+  </ErrorBoundary>
 )
