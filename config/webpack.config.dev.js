0d7a1051bf4678ca6ec17e0576bf7448c47145f4
Альшин Дмитрий Александрович <DAlshin@ibs.root.ibscorp.ru>
2018-10-09 12:25:57 +0300

    Исправлен порядок лоадеров css файлов

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -201,9 +201,6 @@ module.exports = {
                 },
               },
               {
-                loader: require.resolve('sass-loader'),
-              },
-              {
                 loader: require.resolve('postcss-loader'),
                 options: {
                   // Necessary for external CSS imports to work
@@ -222,6 +219,9 @@ module.exports = {
                     }),
                   ],
                 },
+              },
+              {
+                loader: require.resolve('sass-loader'),
               },
             ],
           },
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -236,9 +236,6 @@ module.exports = {
                       },
                     },
                     {
-                      loader: require.resolve('sass-loader'),
-                    },
-                    {
                       loader: require.resolve('postcss-loader'),
                       options: {
                         // Necessary for external CSS imports to work
@@ -257,6 +254,9 @@ module.exports = {
                           }),
                         ],
                       },
+                    },
+                    {
+                      loader: require.resolve('sass-loader'),
                     },
                   ],
                 },
