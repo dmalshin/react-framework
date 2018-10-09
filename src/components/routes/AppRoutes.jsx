b2970b6147c53dcc95a359313810f330ab1728ba
Альшин Дмитрий Александрович <DAlshin@ibs.root.ibscorp.ru>
2018-10-09 10:10:34 +0300

    Добавлен Switch в роуты

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import * as routes from '.'
 
 export const AppRoutes = () => (
   // можно использовать ComponentError из ui-components
   // <ErrorBoundary FallbackComponent={ComponentError}>
   <ErrorBoundary>
-    <Route exact path="/" component={routes.HomeRoute} />
-    <Route path="/ui-components" component={routes.UIComponentsRoute} />
-    <Route path="/form" component={routes.FormRoute} />
+    <Switch>
+      <Route exact path="/" component={routes.HomeRoute} />
+      <Route path="/ui-components" component={routes.UIComponentsRoute} />
+      <Route path="/form" component={routes.FormRoute} />
+    </Switch>
   </ErrorBoundary>
 )
